38508eaa8798af22a2d04a46dc4c6638d07476e2
Aaron Hardy <aahardy@adobe.com>
2018-12-20 20:03:10 -0700

    Fixing minified build file.

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const babel = require('babel-core');
-const uglifyJS = require('uglify-js');
+const Terser = require('terser');
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
@@ -35,10 +35,10 @@ const build = () => {
         });
       },
       (callback) => {
-        fs.readFile(path.join(templateDir, 'amdWeb.template'), 'utf8', function(err, template) {
+        fs.readFile(path.join(templateDir, 'amdWeb.template'), 'utf8', (err, template) => {
           exitIfError(err);
           const umdWrappedCode = template.replace('{{moduleCode}}', result.code);
-          const uglifiedCode = uglifyJS.minify(umdWrappedCode, { fromString: true }).code;
+          const uglifiedCode = Terser.minify(umdWrappedCode).code;
 
           mkdirp.mkdirp(distDir, (err) => {
             exitIfError(err);
