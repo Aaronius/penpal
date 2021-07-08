07e7b85e1ab2fe8f7480e0bc7d4d805d9221da05
Aaron Hardy <aaron@aaronhardy.com>
2021-07-08 15:49:28 -0600

    Fixes #67. Export useful types.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export { default as connectToChild } from './parent/connectToChild';
 export { default as connectToParent } from './child/connectToParent';
 export { ErrorCode } from './enums';
+export {
+  Connection,
+  AsyncMethodReturns,
+  CallSender,
+  Methods,
+  PenpalError,
+} from './types';
